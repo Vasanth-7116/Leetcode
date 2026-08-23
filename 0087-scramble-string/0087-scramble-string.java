1db6b2393b1bb083a1ee007f0d28c40175140529
Vasanth-7116 <vasanthsha997@gmail.com>
2026-08-23 23:52:01 +0530

    Time: 8 ms (48.08%), Space: 47 MB (31.29%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.java
@@ -0,0 +1,50 @@
+class Solution {
+    Map<String, Boolean> memo = new HashMap<>();
+
+    public boolean isScramble(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return true;
+        }
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        
+        String key = s1 + "#" + s2;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        // Pruning: Check if both strings have the same character frequencies
+        int[] count = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                memo.put(key, false);
+                return false;
+            }
+        }
+
+        int n = s1.length();
+        for (int i = 1; i < n; i++) {
+            // Condition 1: Without swap
+            if (isScramble(s1.substring(0, i), s2.substring(0, i)) && 
+                isScramble(s1.substring(i), s2.substring(i))) {
+                memo.put(key, true);
+                return true;
+            }
+            
+            // Condition 2: With swap
+            if (isScramble(s1.substring(0, i), s2.substring(n - i)) && 
+                isScramble(s1.substring(i), s2.substring(0, n - i))) {
+                memo.put(key, true);
+                return true;
+            }
+        }
+
+        memo.put(key, false);
+        return false;
+    }
+}
